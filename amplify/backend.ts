93326b26e22f887e4bcd39185d9fee42687ef683
Eric Shubkagel <eshubkagel@pscu.com>
2025-06-09 15:14:10 -0500

    s3 bucket changes

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,23 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
-defineBackend({
-  auth,
-  data,
+const backend = defineBackend({});
+
+const bucketStack = backend.createStack('CloudAssetsStack');
+const existingBucket = s3.Bucket.fromBucketName(bucketStack, 'HazelAmplifyBucket', 'hazel-amplify');
+
+const distribution = new cloudfront.Distribution(bucketStack, 'HazelAmplifyDistribution', {
+  defaultBehavior: {
+    origin: new origins.S3Origin(existingBucket), // Deprecated, but required for now
+    viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+  },
+  defaultRootObject: 'index.html',
+});
+
+backend.addOutput({
+    custom: {
+      cloudfront_url: `https://${distribution.distributionDomainName}`
+    }
 });
