e07dafe168668ecd1fd2b1c3b9c8a305e38b19c1
eshubkagel-velera <eric.shubkagel@velera.com>
2025-09-16 11:14:16 -0500

    amplify backend update

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,30 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
-// Define backend with auth and data
-const backend = defineBackend({
+defineBackend({
   auth,
-  data
-});
-
-const bucketStack = backend.createStack('CloudAssetsStack');
-const existingBucket = s3.Bucket.fromBucketName(bucketStack, 'HazelAmplifyBucket', 'hazel-amplify');
-
-// Continue using S3Origin for imported buckets
-const distribution = new cloudfront.Distribution(bucketStack, 'HazelAmplifyDistribution', {
-  defaultBehavior: {
-    origin: new origins.S3Origin(existingBucket),
-    viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-  },
-  defaultRootObject: 'index.html',
-});
-
-backend.addOutput({
-  custom: {
-    cloudfront_url: `https://${distribution.distributionDomainName}`
-  }
+  data,
 });
